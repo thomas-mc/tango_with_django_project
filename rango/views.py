4b31a6f36e32225bb919d68328f98e4fc1047330
Thomas McCormick <2315008m@student.gla.ac.uk>
2019-02-01 14:58:21 +0000

    start of chapter 7

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -9,8 +22,6 @@ def index(request):
     context_dict = {'categories': category_list,'pages':pages_list}
 
     return render(request, 'rango/index.html', context_dict)
-
-
 
 def about(request):
     return render(request, 'rango/about.html')
